b966d3e690222fac6c8e74fdb7b0a72caed14a82
Gian U. Uy <gianu@marketamerica.com>
2017-09-12 16:13:55 -0400

    Initial Commit

---
+++ saim-swift-sdk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  saim-swift-sdk
+//
+//  Created by Gian Uy on 9/12/17.
+//  Copyright © 2017 POC Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
